4f94ec14f6d566fb93a700c7e1e7b9f4542853ff
Artem Eluferev <ae@arenadata.io>
2019-05-07 11:34:49 +0300

    ADS-173 bump zookeeper to 3.4.14

--- bigtop-packages/src/common/zookeeper/install_zookeeper.sh
+++ bigtop-packages/src/common/zookeeper/install_zookeeper.sh
@@ -109,10 +109,10 @@ SYSTEM_LIB_DIR=${SYSTEM_LIB_DIR:-/usr/lib}
 install -d -m 0755 $PREFIX/$LIB_DIR/
 rm -f $BUILD_DIR/zookeeper-*-javadoc.jar $BUILD_DIR/zookeeper-*-bin.jar $BUILD_DIR/zookeeper-*-sources.jar $BUILD_DIR/zookeeper-*-test.jar
 cp $BUILD_DIR/zookeeper*.jar $PREFIX/$LIB_DIR/
-install -d -m 0755 ${PREFIX}/${LIB_DIR}/contrib
-for module in rest; do
-    cp -r ${BUILD_DIR}/contrib/${module} ${PREFIX}/${LIB_DIR}/contrib/
-done
+#install -d -m 0755 ${PREFIX}/${LIB_DIR}/contrib
+#for module in rest; do
+#    cp -r ${BUILD_DIR}/contrib/${module} ${PREFIX}/${LIB_DIR}/contrib/
+#done
 
 # Make a symlink of zookeeper.jar to zookeeper-version.jar
 for x in $PREFIX/$LIB_DIR/zookeeper*jar ; do
@@ -128,11 +128,11 @@ install -d -m 0755 $PREFIX/$CONF_DIST_DIR
 cp zoo.cfg $BUILD_DIR/conf/* $PREFIX/$CONF_DIST_DIR/
 ln -s $CONF_DIR $PREFIX/$LIB_DIR/conf
 
-install -d -m 0755 ${PREFIX}/${LIB_DIR}/contrib
-for module in rest; do
-    cp -r ${BUILD_DIR}/contrib/${module} ${PREFIX}/${LIB_DIR}/contrib/
-    mv ${PREFIX}/${LIB_DIR}/contrib/${module}/conf ${PREFIX}/${CONF_DIST_DIR}/${module}
-done
+#install -d -m 0755 ${PREFIX}/${LIB_DIR}/contrib
+#for module in rest; do
+#    cp -r ${BUILD_DIR}/contrib/${module} ${PREFIX}/${LIB_DIR}/contrib/
+#    mv ${PREFIX}/${LIB_DIR}/contrib/${module}/conf ${PREFIX}/${CONF_DIST_DIR}/${module}
+#done
 
 # Copy in the /usr/bin/zookeeper-server wrapper
 install -d -m 0755 $PREFIX/$LIB_DIR/bin
